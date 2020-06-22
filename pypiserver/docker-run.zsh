42b15995b9ba37108ff57728e300df43baecdaf9
JulianFerry <julianferry94@gmail.com>
2020-06-22 09:25:57 +0000

    Package access permissions

--- pypiserver/docker-run.zsh
+++ pypiserver/docker-run.zsh
@@ -6,9 +6,10 @@ docker network inspect pypinet  >/dev/null 2>&1 || docker network create --drive
 # Run container
 echo "Running container pypiserver on port 8080"
 docker run -d \
+  --cap-add SYS_ADMIN \
   --publish 8080:8080 \
-  --volume ~/docker-servers/pypiserver/packages:/data/packages \
+  --volume ~/docker-servers/pypiserver/packages:/data/packages:rw \
   --network pypinet \
   --name pypiserver \
   pypiserver/pypiserver:latest \
-    -P . -a . packages           # pypi-server command options
+    -P . -a . -vv packages           # pypi-server command options
